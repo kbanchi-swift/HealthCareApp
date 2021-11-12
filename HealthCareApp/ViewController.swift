c94d5ea281f0c71a511cd2d67a5a6242ef66023d
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-12 15:13:09 +0900

    Initial Commit

---
+++ HealthCareApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthCareApp
+//
+//  Created by 伴地慶介 on 2021/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
